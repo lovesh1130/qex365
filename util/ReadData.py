3829dc44494f1ac1060e82e20fc7320dac0a39bb
dazzle666 <516384487@qq.com>
2018-10-10 19:11:26 +0800

    数据源读取

--- util/ReadData.py
+++ util/ReadData.py
@@ -2,15 +2,19 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def read_datas():
+def read_datas(startTime, endTime):
     engine = create_engine('mysql+pymysql://root:Quant123@35.162.98.89:3306/quantcoin?charset=utf8MB4')
-    # table = 'btc_kline_60min'
+    #todo 变量输入表名 table = 'btc_kline_60min'
     sql = '''
-        select * from quantcoin.btc_kline_60min;
+        select * from quantcoin.btc_kline_60min
+        where %(start)s <= id
+        and id <= %(endTime)s
         '''
-    df = pd.read_sql_query(sql, engine)
+    df = pd.read_sql_query(sql, params={'start': startTime, 'endTime': endTime}, con=engine)
     return df
 
 
 if __name__ == '__main__':
-    print(read_datas())
+    start = 1508990400
+    end = 1509004800
+    print(read_datas(start, end))
