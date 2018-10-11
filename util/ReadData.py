6eb282f20bd9de6c22b55050391c0fbc2a7ec2ac
dazzle666 <516384487@qq.com>
2018-10-11 18:47:35 +0800

    数据源读取

--- util/ReadData.py
+++ util/ReadData.py
@@ -4,13 +4,13 @@ from sqlalchemy import create_engine
 
 def read_datas(startTime, endTime):
     engine = create_engine('mysql+pymysql://root:Quant123@35.162.98.89:3306/quantcoin?charset=utf8MB4')
-    #todo 变量输入表名 table = 'btc_kline_60min'
+    # todo 变量输入表名 table = 'btc_kline_60min'
     sql = '''
         select * from quantcoin.btc_kline_60min
         where %(start)s <= id
         and id <= %(endTime)s
         '''
-    df = pd.read_sql_query(sql, params={'start': startTime, 'endTime': endTime}, con=engine)
+    df = pd.read_sql_query(sql, params={'start': startTime, 'endTime': endTime}, con=engine, index_col='kline_id')
     return df
 
 
